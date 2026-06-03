ccea73f86471fca1ea601b900859dc13c295de51
mijohnst <mijohnst@gmail.com>
2026-06-03 14:24:42 -0500

    Adding photo-proxy.html.

---
+++ photo-proxy.php
@@ -0,0 +1,6 @@
+<?php
+header('Content-Type: application/json');
+$url = 'https://script.google.com/macros/s/AKfycbyJgBxqksYcbPKnvCOMc7I1SZbgJbM-v6A1BaeSn0mr8aRRZMuSpJwuceOij4cAesct/exec';
+$ctx = stream_context_create(['http' => ['timeout' => 10, 'follow_location' => true]]);
+$data = @file_get_contents($url, false, $ctx);
+echo $data ?: '{"images":[]}';
