8692076110c770d823b2cd20b231645c56093434
mijohnst <mijohnst@gmail.com>
2026-06-04 10:48:04 -0500

    Fixed sitewide errors.

--- photo-proxy.php
+++ photo-proxy.php
@@ -1,6 +1,6 @@
 <?php
-header('Content-Type: application/json');
+header('Content-Type: application/javascript');
 $url = 'https://script.google.com/macros/s/AKfycbyJgBxqksYcbPKnvCOMc7I1SZbgJbM-v6A1BaeSn0mr8aRRZMuSpJwuceOij4cAesct/exec';
 $ctx = stream_context_create(['http' => ['timeout' => 10, 'follow_location' => true]]);
 $data = @file_get_contents($url, false, $ctx);
-echo $data ?: '{"images":[]}';
+echo $data ?: '_photosReady([])';
